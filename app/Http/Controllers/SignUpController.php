3cdfa15bbe48f253b23e0af459a9aea6cb5a9ad7
HakanSy <hakansehattt@gmail.com>
2024-07-12 13:41:07 +0700

    hwegfwue

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -12,7 +12,7 @@ class SignUpController extends Controller
 {
     public function index()
     {
-        return view('signUp');
+        return view('signup');
     }
     
     public function store(Request $request)
